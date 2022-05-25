af7fd4ad546655eb93637ed0618e198a4fd5a0d4
Boryana Dimitrova <b.dimitrova97@gmail.com>
2022-05-25 16:02:44 +0300

    Set Database

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 
 const { PORT } = require('./constants');
+const routes = require('./routes');
+
+const initDatabase = require('./config/mongooseConfig');
 
 const app = express();
 
-const routes = require('./routes');
 
 require('./config/hbsConfig')(app);
 require('./config/expressConfig')(app);
+
 app.use(routes);
 
-app.listen(PORT, () => console.log(`The app is running on http://localhost:${PORT}`));
+initDatabase()
+    .then(() => {
+        app.listen(PORT, () => console.log(`The app is running on http://localhost:${PORT}`));
+    })
+    .catch((err) => {
+        console.log('Cannot connect database:', err);
+    })
