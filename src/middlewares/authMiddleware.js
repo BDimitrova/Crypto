d062d57adfd24ae437a0f8973948759322de12e3
Boryana Dimitrova <b.dimitrova97@gmail.com>
2022-05-26 16:53:13 +0300

    Update Middleware.js

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,5 +1,20 @@
+const jwt = require('../utils/jwt');
+
 const {AUTH_COOKIE_NAME} = require('../constants')
 
 exports.auth = function(req, res, next) {
-    let cookie = req.cookies[AUTH_COOKIE_NAME];
+    let token = req.cookies[AUTH_COOKIE_NAME];
+
+    if(token) {
+        jwt.verify(token)
+            .then(decodedToken => {
+                req.user = decodedToken;
+            })
+            .catch(err => {
+                res.clearCookie(AUTH_COOKIE_NAME);
+                res.status(401).render('404');
+            })
+    } else {
+        next();
+    }
 }
